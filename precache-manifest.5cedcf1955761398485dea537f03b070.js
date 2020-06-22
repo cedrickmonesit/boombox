7329286c42df58780a4f144715a5057ffe01bc79
Cedrick Monesit <cedrickmonesit@gmail.com>
2020-06-22 14:16:09 -0400

    Updates

---
+++ precache-manifest.5cedcf1955761398485dea537f03b070.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342f793fc2fcc712d82d284746bb4234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "501c0d8a04db1777deb5",
+    "url": "/static/css/2.440ac8e8.chunk.css"
+  },
+  {
+    "revision": "d3604445ce3138715b34",
+    "url": "/static/css/main.df251436.chunk.css"
+  },
+  {
+    "revision": "501c0d8a04db1777deb5",
+    "url": "/static/js/2.fb6e8310.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fb6e8310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3604445ce3138715b34",
+    "url": "/static/js/main.7a937e09.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "e311bf2ca733f63e1dfc2da94493b693",
+    "url": "/static/media/logo.e311bf2c.png"
+  }
+]);
